6b55b4a55babc58f2d3b08fb3ec6574b6edec094
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2025-04-04 16:02:34 -0300

    Add DbSet properties and seed data in AppDbContext
    - Added `using` directive for `ListaDeTarefas.Models`.
    - Added `DbSet` properties for `Tarefas`, `Categorias`, and `Statuses`.
    - Overrode `OnModelCreating` to seed initial data for `Categoria` and `Status`.
    - Used `modelBuilder.Entity<Categoria>().HasData` and `modelBuilder.Entity<Status>().HasData` to insert predefined records.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ListaDeTarefas.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ListaDeTarefas.Data
 {
@@ -7,6 +8,31 @@ namespace ListaDeTarefas.Data
         public AppDbContext(DbContextOptions<AppDbContext> option) : base(option)
         {
             
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>().HasData(
+                    new Categoria { CategoriaId = "trabalho", Nome = "Trabalho" },
+                    new Categoria { CategoriaId = "casa", Nome = "Casa" },
+                    new Categoria { CategoriaId = "faculdade", Nome = "Faculdade" },
+                    new Categoria { CategoriaId = "compras", Nome = "Academia" },
+                    new Categoria { CategoriaId = "academia", Nome = "Academia" }
+
+                );
+
+            modelBuilder.Entity<Status>().HasData(
+                    new Status { StatusId = "aberto", Nome = "Aberto" },
+                    new Status { StatusId = "completo", Nome = "Completo" }
+                );
+
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
